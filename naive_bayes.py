ab95e2157e1d5ec89faedb8d201de0be93bd6274
madhuvijay95 <madhuvijay95@gmail.com>
2016-11-25 23:34:17 -0500

    naive bayes: misc error fixes

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,22 +1,25 @@
+import numpy as np
+import scipy as sp
+
 class NaiveBayes:
-    def train(self, X, y, alpha):
+    def fit(self, X, y, alpha=1):
         self.X = np.array(X)
         self.y = np.array(y)
         self.classes = list(set(self.y))
+        self.classes_rev = {v:k for k,v in enumerate(self.classes)}
         self.nclasses = len(self.classes)
 
-        self.classcounts = [float((y==c).sum()) for c in self.classes]
-        self.classprobs = (self.classcounts.T / self.classcounts.sum(axis==1)).T
+        self.classcounts = np.array([float((self.y==c).sum()) for c in self.classes])
+        self.classprobs = self.classcounts / self.classcounts.sum()
         self.logclassprobs = np.log(self.classprobs)
 
-        self.wordcounts = np.array([float((X[y==c] + alpha).sum(axis=0)) for c in self.classes])
-        self.wordprobs = (self.wordcounts.T / self.wordcounts.sum(axis=1)).T
+        self.wordcounts = np.array([map(float, np.array((X[y==c].todense()).sum(axis=0))[0]) for c in self.classes])
+        self.wordprobs = ((self.wordcounts + alpha).T / (self.wordcounts + alpha).sum(axis=1)).T
         self.logwordprobs = np.log(self.wordprobs)
 
     def predict_log_proba(self, X):
-        log_proba = np.dot(X, self.logwordprobs.T)
+        log_proba = X.dot(self.logwordprobs.T)
         log_proba += self.logclassprobs
-        log_proba -= np.log(np.exp(log_proba))
         log_proba = (log_proba.T - np.log(np.exp(log_proba).sum(axis=1))).T
         return log_proba
 
@@ -32,22 +35,27 @@ class NaiveBayes:
     def score(self, X, y):
         predictions = self.predict(X)
         n_correct = sum([p==c for p, c in zip(predictions, y)])
-        return float(n_correct) / X.shape[1]
+        return float(n_correct) / X.shape[0]
+
+    def score2(self, X, y):
+        proba = self.predict_proba(X)
+        y_ind = [self.classes_rev[c] for c in y]
+        return np.mean([row[ind] for row, ind in zip(proba, y_ind)])
 
     def cross_validation(self, X, y, alphas, k=5):
-        n_samples = X.shape[1]
+        n_samples = X.shape[0]
         assert(len(y) == n_samples)
         indices = range(n_samples)
         np.random.shuffle(indices)
         split = [sorted(indices[start::k]) for start in range(k)]
-        split_X = [np.array(X)[ind_list] for ind_list in split]
+        split_X = [X[ind_list] for ind_list in split]
         split_y = [np.array(y)[ind_list] for ind_list in split]
         scores = dict()
         for alpha in alphas:
             scores[alpha] = []
             for leave_out in range(k):
-                X_train = np.hstack(tuple(split_X[i] for i in range(k) if i != leave_out))
+                X_train = sp.sparse.vstack(tuple(split_X[i] for i in range(k) if i != leave_out))
                 y_train = np.concatenate(tuple(split_y[i] for i in range(k) if i != leave_out))
-                self.train(X_train, y_train, alpha)
+                self.fit(X_train, y_train, alpha)
                 scores[alpha].append(self.score(split_X[leave_out], split_y[leave_out]))
         return scores
