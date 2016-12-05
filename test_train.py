ff2e5be99982e10a4a1569a121e1f52e45947c7c
Yunhan Xu <yunhanxu@Yunhans-MacBook-Air.local>
2016-12-04 19:19:31 -0500

    test train split code and csvs - train.csv and test.csv (70/30)

---
+++ test_train.py
@@ -0,0 +1,18 @@
+# split dataframe into test and train
+# for cross-algorithm testing
+
+import pandas as pd
+import numpy as np
+
+# import data
+filename = 'augmented.csv'
+df = pd.read_csv(filename)
+del df['Unnamed: 0']
+
+# 70/30 split for train/test
+msk = np.random.rand(len(df)) < 0.7
+train = df[msk]
+test = df[~msk]
+
+train.to_csv("train.csv", index=False)
+test.to_csv("test.csv", index=False)
